cc2e454a8e6cb5c48f841385979451b24131f8d9
tomalex0 <tomalex0@gmail.com>
2017-05-27 14:56:24 -0700

    update stats

---
+++ js/schedule.js
@@ -0,0 +1,137 @@
+var datetime = Math.round(new Date().getTime() / 1000);
+
+var players = [];
+var cautions = [];
+var playerobj = {};
+var cautionobj = {};
+
+loadPlayers();
+loadCautions();
+
+$(document).ready(function () {
+
+    loadTeams(loadSchedule)
+
+
+});
+
+function getTeamName (teamsArr, teamId) {
+    return _.where(teamsArr,{id : teamId})[0];
+}
+
+
+
+function getPlayer (item){
+    return playerobj[item['player']][0];
+}
+
+function getCautions (stats) {
+    var cautions = []
+    if(stats.cautions) {
+        cautions = _.map(stats.cautions, function(item){
+            return {
+                player : getPlayer(item),
+                card   : cautionobj[item['caution_id']][0],
+                minute : item.minute
+            }
+        })
+    }
+    return cautions;
+}
+
+function getGoals (stats) {
+    var goals = []
+    if(stats.goals) {
+        goals = _.map(stats.goals, function(item){
+            return {
+                player : getPlayer(item),
+                minute : item.minute,
+                owngoal : item.owngoal
+            }
+        })
+    }
+    return goals;
+}
+
+function  loadTeams (callback){
+    $.getJSON('./data/teams.json?time='+datetime,function(json){
+        callback(json)
+    });
+}
+function  loadPlayers (){
+    $.getJSON('./data/players.json?time='+datetime,function(json){
+        players = json;
+    });
+}
+
+function  loadCautions (){
+    $.getJSON('./data/caution.json?time='+datetime,function(json){
+        cautions = json;
+    });
+}
+
+function buildTableData (teamsArr, scheduleArr){
+
+    playerobj = _.groupBy(players,function (o) {
+        return o.id;
+    });
+    cautionobj = _.groupBy(cautions,function (o) {
+        return o.id;
+    });
+    var scheduleArrFormatted = [];
+    _.each(scheduleArr,function(item, index){
+        var newItem = JSON.parse(JSON.stringify(item));
+
+
+        newItem.referees = _.map(item.referees,function(itemObj){
+
+            return {
+                team :  getTeamName(teamsArr, itemObj.team),
+                player :  (itemObj.player) ? getPlayer(itemObj) : undefined
+            };
+
+        });
+
+
+        newItem.home_team =  getTeamName(teamsArr, item.stats.home.team);
+        newItem.away_team =  getTeamName(teamsArr, item.stats.away.team);
+
+        newItem.home_mom = _.map(item.stats.home.mom,function(itemobj){
+            return  getPlayer(itemobj)
+        });
+        newItem.away_mom =  _.map(item.stats.away.mom,function(itemobj){
+            return  getPlayer(itemobj)
+        });
+
+        newItem.home_cautions = (item.stats) ? getCautions(item.stats.home) : [];
+        newItem.away_cautions = (item.stats) ?  getCautions(item.stats.away) : [];
+
+        newItem.home_goals = (item.stats) ? getGoals(item.stats.home) : undefined;
+        newItem.away_goals = (item.stats) ?  getGoals(item.stats.away) : undefined;
+
+        //console.log(newItem,'0---------------')
+        scheduleArrFormatted.push(newItem)
+    });
+
+    return scheduleArrFormatted;
+
+}
+
+function  renderTable (scheduleData){
+
+    var schedule_source   = $("#schedule-template").html();
+    var schedule_template = Handlebars.compile(schedule_source);
+
+
+    var schedule_html    = schedule_template(scheduleData);
+    $('tbody').html(schedule_html);
+
+}
+var scheduleData
+function  loadSchedule(teamsArr){
+    $.getJSON('./data/schedule.json?time='+datetime,function(json){
+        scheduleData = buildTableData(teamsArr, json);
+        renderTable(scheduleData);
+    });
+
+}
