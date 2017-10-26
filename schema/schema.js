862bda59b94edc8642d2ba8a33e4bc2c6928bde4
tomalex0 <tomalex0@gmail.com>
2017-10-25 22:51:13 -0700

    move root query to separate file

--- schema/schema.js
+++ schema/schema.js
@@ -1,102 +1,10 @@
 'use strict';
 
-const _ = require('lodash');
-
-
-const TeamType = require('./team-schema');
-const PlayerType = require('./player-schema');
-const CautionType = require('./caution-schema');
-const SeasonType = require('./season-schema');
-const ScheduleType = require('./schedule-schema');
-
-const PlayersController = require('../controllers/players');
-const playersCtrl  = new PlayersController();
-
-const TeamsController = require('../controllers/teams');
-const teamsCtrl  = new TeamsController();
-
-const SeasonsController = require('../controllers/seasons');
-const seasonsCtrl  = new SeasonsController();
-
-const CautionsController = require('../controllers/cautions');
-const cautionsCtrl  = new CautionsController();
-
-const ScheduleController = require('../controllers/schedule');
-const scheduleCtrl  = new ScheduleController();
-
-
-
-
-/* Here a simple schema is constructed without using the GraphQL query language.
- e.g. using 'new GraphQLObjectType' to create an object type
- */
+const MorninigSoccerQueryRootType = require('./root-query-type');
 
 let {
-    // These are the basic GraphQL types need in this tutorial
-    GraphQLString,
-    GraphQLList,
-    GraphQLInt,
-    GraphQLObjectType,
-    // This is used to create required fileds and arguments
-    GraphQLNonNull,
-    // This is the class we need to create the schema
     GraphQLSchema,
     } = require('graphql');
-
-
-// This is the Root Query
-const MorninigSoccerQueryRootType = new GraphQLObjectType({
-        name: 'MorningSoccerAppSchema',
-        description: "MorninigSoccer Application Schema Query Root",
-        fields: () => ({
-            teams: {
-                type: new GraphQLList(TeamType),
-                description: "List of all Teams",
-                args:{
-                    teams :{type:new GraphQLList(GraphQLInt)}
-                },
-                resolve: function(root, args) {
-                    let teamList = (args.teams)
-                                    ? teamsCtrl.getList(args.teams)
-                                    : teamsCtrl.getList()
-                    return teamList;
-                }
-            },
-            seasons: {
-                type: new GraphQLList(SeasonType),
-                args:{
-                    seasons :{type:new GraphQLList(GraphQLInt)}
-                },
-                description: "List of all Seasons",
-                resolve: function(root,args) {
-                    return  (args.seasons)
-                            ? seasonsCtrl.getList(args.seasons)
-                            : seasonsCtrl.getList()
-                }
-            },
-            players: {
-                type: new GraphQLList(PlayerType),
-                description: "List of all Players",
-                resolve: function(args) {
-                    return playersCtrl.getList();
-                }
-            },
-            cautions: {
-                type: new GraphQLList(CautionType),
-                description: "List of all Cautions",
-                resolve: function(args) {
-                    return cautionsCtrl.getList();
-                }
-            },
-            schedule: {
-                type: new GraphQLList(ScheduleType),
-                description: "List of all Schedule",
-                resolve: function(args) {
-                    return scheduleCtrl.getList();
-                }
-            }
-        })
-});
 
 // This is the schema declaration
 const MorningSoccerAppSchema = new GraphQLSchema({
