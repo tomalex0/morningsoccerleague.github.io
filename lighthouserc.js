a8f7f75db4360c52a17d0447a681e031f9744e47
tomalex0 <tomalex0@gmail.com>
2020-11-25 04:01:52 -0800

    set prefix path part of npm script

--- lighthouserc.js
+++ lighthouserc.js
@@ -22,6 +22,7 @@ module.exports = {
         "non-composited-animations": "off",
         "tap-targets": "off",
         "errors-in-console": "off",
+        "max-potential-fid": "off",
       },
     },
   },
