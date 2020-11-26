a75a242eeac77b12bc4f83336b4a2519e04c2bef
tomalex0 <tomalex0@gmail.com>
2020-11-25 20:28:35 -0800

    add twitter image

--- src/components/seo.js
+++ src/components/seo.js
@@ -89,7 +89,7 @@ function SEO({ description, lang, meta, title, image, path }) {
         },
         {
           name: `twitter:card`,
-          content: `summary`,
+          content: `summary_large_image`,
         },
         {
           name: `twitter:creator`,
