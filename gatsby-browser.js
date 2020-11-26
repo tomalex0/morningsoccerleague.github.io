48a81b90f8dc9813bc60b27dd0c55d1a86f45ef6
tomalex0 <tomalex0@gmail.com>
2020-11-26 15:10:57 -0800

    add profiler code for build

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -3,7 +3,35 @@
  *
  * See: https://www.gatsbyjs.com/docs/browser-apis/
  */
+import * as React from "react"
+import { Profiler } from "react"
 import "./src/css/index.css"
-// You can delete this file if you're not using it
+function capturePageMetrics(
+  id, // the "id" prop of the Profiler tree that has just committed
+  phase, // either "mount" (if the tree just mounted) or "update" (if it re-rendered)
+  actualDuration, // time spent rendering the committed update
+  baseDuration, // estimated time to render the entire subtree without memoization
+  startTime, // when React began rendering this update
+  commitTime, // when React committed this update
+  interactions // the Set of interactions belonging to this update
+) {
+  // Aggregate or log render timings...
+  console.table({
+    id,
+    phase,
+    actualDuration,
+    baseDuration,
+    startTime,
+    commitTime,
+    interactions
+  })
+}
+
+export const wrapPageElement = ({ element, props }) => (
+  // See https://reactjs.org/docs/profiler.html#onrender-callback for onRender parameters
+  <Profiler id={props.someUniqueId} onRender={capturePageMetrics}>
+    {element}
+  </Profiler>
+)
 
 export const registerServiceWorker = () => true
