6bd6824320df3792d638d023e1716f4a5099a141
tomalex0 <tomalex0@gmail.com>
2020-11-27 02:48:36 -0800

    run lhci after deployment

---
+++ lighthouserc-url.js
@@ -0,0 +1,30 @@
+module.exports = {
+  ci: {
+    collect: {
+      numberOfRuns: 1,
+      url: "https://tomalex0.github.io/morningsoccerleague.github.io/",
+      isSinglePageApplication: true,
+      settings: {
+        chromeFlags: " --no-sandbox --disable-dev-shm-usage",
+      },
+    },
+    upload: {
+      target: "temporary-public-storage",
+    },
+    assert: {
+      preset: "lighthouse:no-pwa",
+      assertions: {
+        "uses-rel-preload": "off",
+        "uses-rel-preconnect": "off",
+        "robots-txt": "off",
+        "legacy-javascript": "off",
+        "unused-javascript": "off",
+        "non-composited-animations": "off",
+        "tap-targets": "off",
+        "errors-in-console": "off",
+        "max-potential-fid": "off",
+        "is-crawlable": "off",
+      },
+    },
+  },
+}
