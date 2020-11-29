332546237978aeaafc870dfa53d83eb43b3d24df
tomalex0 <tomalex0@gmail.com>
2020-11-29 00:08:55 -0800

    add seasons to teams data

--- graphql/resolvers/msl.js
+++ graphql/resolvers/msl.js
@@ -1,4 +1,22 @@
 module.exports = {
+  MslTeamsJson: {
+    seasons: {
+      type: ["MslSeasonsJson"],
+      resolve(source, args, context, info) {
+        return context.nodeModel.runQuery({
+          query: {
+            filter: {
+              teams: {
+                elemMatch: { team: { team_id: { eq: source.team_id } } },
+              },
+            },
+          },
+          type: "MslSeasonsJson",
+          firstOnly: false,
+        })
+      },
+    },
+  },
   MslPlayersJson: {
     mos: {
       type: ["MslSeasonsJson"],
