d79ad53a16ca75483f75e2d38fb5b43de3cb1dc9
tomalex0 <tomalex0@gmail.com>
2020-11-30 23:54:47 -0800

    remove unused functions and comment types

--- graphql/resolvers/msl.js
+++ graphql/resolvers/msl.js
@@ -3,6 +3,7 @@ module.exports = {
     seasons: {
       type: ["MslSeasonsJson"],
       resolve(source, args, context, info) {
+        console.log(source, "---seasons---")
         return context.nodeModel.runQuery({
           query: {
             filter: {
@@ -12,94 +13,6 @@ module.exports = {
             },
           },
           type: "MslSeasonsJson",
-          firstOnly: false,
-        })
-      },
-    },
-  },
-  MslSeasonsJson: {
-    schedules: {
-      type: ["MslSchedulesJson"],
-      resolve(source, args, context, info) {
-        return context.nodeModel.runQuery({
-          query: {
-            filter: {
-              season: {
-                season_id: { eq: source.season_id },
-              },
-            },
-          },
-          type: "MslSchedulesJson",
-          firstOnly: false,
-        })
-      },
-    },
-  },
-  MslPlayersJson: {
-    mos: {
-      type: ["MslSeasonsJson"],
-      resolve(source, args, context, info) {
-        return context.nodeModel.runQuery({
-          query: {
-            filter: {
-              mos: {
-                elemMatch: {
-                  player_id: {
-                    eq: source.player_id,
-                  },
-                },
-              },
-            },
-          },
-          type: "MslSeasonsJson",
-          firstOnly: false,
-        })
-      },
-    },
-    owners: {
-      type: ["MslSeasonsJsonTeams"],
-      resolve(source, args, context, info) {
-        return context.nodeModel.runQuery({
-          query: {
-            filter: {
-              teams: {
-                elemMatch: {
-                  owners: {
-                    elemMatch: {
-                      player_id: {
-                        eq: source.player_id,
-                      },
-                    },
-                  },
-                },
-              },
-            },
-          },
-          type: "MslSeasonsJsonTeams",
-          firstOnly: false,
-        })
-      },
-    },
-    players: {
-      type: ["MslSeasonsJsonTeams"],
-      resolve(source, args, context, info) {
-        return context.nodeModel.runQuery({
-          query: {
-            filter: {
-              teams: {
-                elemMatch: {
-                  players: {
-                    elemMatch: {
-                      player_id: {
-                        eq: source.player_id,
-                      },
-                    },
-                  },
-                },
-              },
-            },
-          },
-          type: "MslSeasonsJsonTeams",
           firstOnly: false,
         })
       },
