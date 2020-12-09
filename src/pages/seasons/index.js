35d4bfcbc6b2029882784419ec434b1fd11fd175
tomalex0 <tomalex0@gmail.com>
2020-12-09 00:54:35 -0800

    add season info

--- src/pages/seasons/index.js
+++ src/pages/seasons/index.js
@@ -10,6 +10,13 @@ import {
   MslSeasonsJsonFragment,
 } from "data/fragments"
 
+function getTotalPlayers(teams) {
+  let sum = 0
+  teams.map(item => {
+    sum += item.players.length
+  })
+  return sum
+}
 const SeasonsIndex = ({ data, path }) => {
   const { seasons } = data
   return (
@@ -23,7 +30,9 @@ const SeasonsIndex = ({ data, path }) => {
         {seasons.nodes.map(season => (
           <li key={season.season_id}>
             <Link to={season.seasonPath}>
-              {season.season_id}-{season.season_year}
+              {season.season_id}-{season.season_year}-{season.schedules.length}{" "}
+              Game--{season.teams.length} Teams --{" "}
+              {getTotalPlayers(season.teams)} Players
             </Link>
           </li>
         ))}
@@ -37,6 +46,14 @@ export const query = graphql`
     seasons: allMslSeasonsJson(sort: { order: ASC, fields: season_id }) {
       nodes {
         ...MslSeasonsJsonFragment
+        schedules {
+          id
+        }
+        teams {
+          players {
+            id
+          }
+        }
       }
     }
   }
