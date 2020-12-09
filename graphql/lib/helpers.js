a5b2e8a7a567b659a3b8434bde0fb6e0306defa7
tomalex0 <tomalex0@gmail.com>
2020-12-08 21:50:08 -0800

    add player, team, owner info

--- graphql/lib/helpers.js
+++ graphql/lib/helpers.js
@@ -9,3 +9,21 @@ exports.getFile = (context, path) => {
   const record = data.find(item => item.relativePath == path)
   return record
 }
+
+// findInNested(12, [], 'players')
+// [{
+//  	"players": [1, 2, 3]
+//  }, {
+//  	"players": [12, 13]
+//  }]
+exports.findInNested = (searchData, arr, parentProp, accum = [], parentArr) => {
+  arr.forEach(f => {
+    if (f[parentProp]) {
+      findInNested(searchData, f[parentProp], parentProp, accum, f)
+    }
+    if (f == searchData) {
+      accum.push(parentArr)
+    }
+  })
+  return accum
+}
--- graphql/resolvers/msl.js
+++ graphql/resolvers/msl.js
@@ -1,5 +1,5 @@
 const { MslImgKey } = require("../lib/enum")
-const { getFile } = require("../lib/helpers")
+const { getFile, findInNested } = require("../lib/helpers")
 
 module.exports = {
   MslTeamsJson: {
@@ -37,11 +37,100 @@ module.exports = {
         return record
       },
     },
+    seasons: {
+      type: ["MslSeasonsJson"],
+      async resolve(source, args, context, info) {
+        const data = await context.nodeModel.runQuery({
+          query: {
+            filter: {
+              teams: {
+                elemMatch: {
+                  players: {
+                    elemMatch: { player_id: { eq: source.player_id } },
+                  },
+                },
+              },
+            },
+          },
+          type: "MslSeasonsJson",
+          firstOnly: false,
+        })
+        if (source.player_id) {
+          // Add player_id to each season, so that player_id will available at MslSeasonsJson
+          data.map(item => {
+            item.player_id = source.player_id
+          })
+        }
+
+        return data
+      },
+    },
+    mos: {
+      type: ["MslSeasonsJson"],
+      async resolve(source, args, context, info) {
+        const data = await context.nodeModel.runQuery({
+          query: {
+            filter: {
+              mos: {
+                elemMatch: {
+                  player_id: {
+                    eq: source.player_id,
+                  },
+                },
+              },
+            },
+          },
+          type: "MslSeasonsJson",
+          firstOnly: false,
+        })
+        return data
+      },
+    },
   },
   MslSchedulesJson: {
     stats: {
       resolve(source, args, context, info) {
         return source.gamestats.find(item => item.team_type == args.stats_type)
+      },
+    },
+  },
+  MslSeasonsJson: {
+    // Depends on Player ID
+    isMos: {
+      resolve(source, args, context, info) {
+        var isMos = false
+        if (source.player_id) {
+          isMos = source.mos.includes(source.player_id)
+        }
+        return isMos
+      },
+    },
+    teamInfo: {
+      resolve(source, args, context, info) {
+        const teams = source.teams
+        const playerId = source.player_id
+        const playerTeam = findInNested(playerId, teams, "players")
+        const playerOwner = findInNested(playerId, teams, "owners")
+        return {
+          team: playerTeam.length > 0 ? playerTeam[0].team : null,
+          isOwner: playerOwner.length > 0 ? true : false,
+        }
+      },
+    },
+    schedules: {
+      type: ["MslSchedulesJson"],
+      resolve(source, args, context, info) {
+        return context.nodeModel.runQuery({
+          query: {
+            filter: {
+              season: {
+                season_id: { eq: source.season_id },
+              },
+            },
+          },
+          type: "MslSchedulesJson",
+          firstOnly: false,
+        })
       },
     },
   },
