891500fe6b428ea41fdbe4de59f81a4ddcc4ad8e
tomalex0 <tomalex0@gmail.com>
2020-12-10 19:21:26 -0800

    change theme color

--- gatsby-config.js
+++ gatsby-config.js
@@ -77,7 +77,7 @@ module.exports = {
         lang: `en`,
         start_url: `/`,
         background_color: `#000000`,
-        theme_color: `#A4281D`,
+        theme_color: `#000000`,
         display: `minimal-ui`,
         icon: `src/images/logo.svg`, // This path is relative to the root of the site.
         icon_options: {
