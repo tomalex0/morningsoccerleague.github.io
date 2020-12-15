3a0594b72f6d3d9782126a6bb20b9c9fece499d8
tomalex0 <tomalex0@gmail.com>
2020-12-15 00:58:09 -0800

    add player stats component

--- src/components/msl/player-stats-item.js
+++ src/components/msl/player-stats-item.js
@@ -8,14 +8,14 @@ const MslPlayerStatsItem = ({ player }) => {
         {player.player_id} - {player.name}
       </Link>
       <span>
-              {player.playerStats.allseasonStats.goals} Total Goals ---
+        {player.playerStats.allseasonStats.goals} Total Goals ---
         {player.playerStats.allseasonStats.assists} Total Assists---
         {player.playerStats.allseasonStats.mos} Total Mos---
         {player.playerStats.allseasonStats.yellow_cards} Total Yellow --
         {player.playerStats.allseasonStats.red_cards} Total Red --
         {player.playerStats.allseasonStats.mom} Total Mom --
         {player.playerStats.allseasonStats.saves} Total Saves --
-            </span>
+      </span>
     </li>
   )
 }
