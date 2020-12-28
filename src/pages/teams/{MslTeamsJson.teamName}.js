f73fae2a0873a440f6eae5551c72513131f6a278
tomalex0 <tomalex0@gmail.com>
2020-12-28 00:29:00 -0800

    add team stats

--- src/pages/teams/{MslTeamsJson.teamName}.js
+++ src/pages/teams/{MslTeamsJson.teamName}.js
@@ -1,5 +1,5 @@
 import React from "react"
-import { graphql } from "gatsby"
+import { graphql, Link } from "gatsby"
 
 import Layout from "components/layout"
 import Image from "components/image"
@@ -9,17 +9,30 @@ import {
   MslPlayersJsonFragment,
   MslTeamsJsonFragment,
   MslSeasonsJsonFragment,
+  MslSeasonsJsonStatsFragment,
 } from "data/fragments"
 
 function Team({ path, data }) {
   const { team } = data
-
+  // console.log(team,'--54---')
   return (
     <Layout>
       <SEO title={team.teamName} path={path} />
       <h1>
         Hi Team {team.teamName} - {team.team_id}
       </h1>
+      <div>
+        <h3>Seasons Played</h3>
+        <ul>
+          {team.seasons.map(stats => (
+            <li key={stats.season_id}>
+              <Link to={stats.seasonPath}>
+                {stats.season_id} - {stats.season_year}
+              </Link>
+            </li>
+          ))}
+        </ul>
+      </div>
       <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
         <Image />
       </div>
@@ -31,6 +44,9 @@ export const query = graphql`
   query($id: String) {
     team: mslTeamsJson(id: { eq: $id }) {
       ...MslTeamsJsonFragment
+      seasons {
+        ...MslSeasonsJsonStatsFragment
+      }
     }
   }
 `
