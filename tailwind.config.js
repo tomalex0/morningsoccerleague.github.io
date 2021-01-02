4ba13960e1726a99a74cdb788d65e8788d1cd659
tomalex0 <tomalex0@gmail.com>
2021-01-02 02:22:08 -0800

    add hero and implement dark and light mode

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,4 +1,5 @@
-const IS_GITHUB = process.env.CI === true
+const IS_GITHUB = process.env.GITHUB_ACTIONS === true
+console.log(IS_GITHUB, "-IS_GITHUB-", process.env.GITHUB_ACTIONS)
 module.exports = {
   purge: {
     enabled: IS_GITHUB,
