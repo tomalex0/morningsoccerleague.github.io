924fdf8cce96e3db92215844a71ae6208b8fdc4b
tomalex0 <tomalex0@gmail.com>
2021-01-03 13:58:08 -0800

    update player page

--- src/components/msl/hero.js
+++ src/components/msl/hero.js
@@ -32,7 +32,7 @@ const MslHero = () => {
               <div className="mt-5 sm:mt-8 sm:flex sm:justify-center lg:justify-start">
                 <div className="rounded-md shadow">
                   <Link
-                    to="/stats"
+                    to="/seasons"
                     className="w-full flex items-center justify-center px-8 py-3 border border-transparent text-base font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 md:py-4 md:text-lg md:px-10"
                   >
                     Check Seasons
