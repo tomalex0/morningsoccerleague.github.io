f6c4c85303f86dfa614814687cd68101e2899043
tomalex0 <tomalex0@gmail.com>
2021-01-03 10:41:49 -0800

    update teams page

--- src/pages/teams/{MslTeamsJson.teamName}.js
+++ src/pages/teams/{MslTeamsJson.teamName}.js
@@ -3,7 +3,7 @@ import { graphql, Link } from "gatsby"
 
 import Layout from "components/layout"
 import SEO from "components/seo"
-
+import MslTotalStatsItem from "components/msl/total-stats-item"
 import {
   MslPlayersJsonFragment,
   MslTeamsJsonFragment,
@@ -19,14 +19,154 @@ function Team({ path, data }) {
   return (
     <Layout>
       <SEO title={team.teamName} path={path} />
-      <h1>
-        Hi Team {team.teamName} - {team.team_id}
-      </h1>
-      <div>
-        Total Games, {allSeasonStats.goals}-Total Goals, {allSeasonStats.mos}
-        -Total Mos, {allSeasonStats.yellow_cards}-Total Yellow,{" "}
-        {allSeasonStats.red_cards}-Total Red, {allSeasonStats.mom}-Total Mom,
-        {allSeasonStats.players} - Unique Total Players
+      <div className="lg:flex lg:items-center lg:justify-between px-5 mt-5">
+        <div className="flex-1 min-w-0">
+          <h1 className="text-2xl font-bold leading-7 text-gray-600 dark:text-gray-300  sm:text-3xl sm:truncate">
+            Team {team.teamName}
+          </h1>
+          <div className="mt-4 grid grid-cols-2 md:grid-cols-4 lg:grid-cols-5 xl:grid-cols-8 gap-4">
+            <MslTotalStatsItem
+              title={`Total Games`}
+              value={allSeasonStats.goals}
+            >
+              <svg
+                className="flex-shrink-0 h-6 w-6 text-indigo-600 dark:text-indigo-400"
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke="currentColor"
+                aria-hidden="true"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth={2}
+                  d="M9 19v-6a2 2 0 00-2-2H5a2 2 0 00-2 2v6a2 2 0 002 2h2a2 2 0 002-2zm0 0V9a2 2 0 012-2h2a2 2 0 012 2v10m-6 0a2 2 0 002 2h2a2 2 0 002-2m0 0V5a2 2 0 012-2h2a2 2 0 012 2v14a2 2 0 01-2 2h-2a2 2 0 01-2-2z"
+                />
+              </svg>
+            </MslTotalStatsItem>
+            <MslTotalStatsItem
+              title={`Total Goals`}
+              value={allSeasonStats.goals}
+            >
+              <svg
+                className="flex-shrink-0 h-6 w-6 text-indigo-600 dark:text-indigo-400"
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke="currentColor"
+                aria-hidden="true"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth={2}
+                  d="M9 19v-6a2 2 0 00-2-2H5a2 2 0 00-2 2v6a2 2 0 002 2h2a2 2 0 002-2zm0 0V9a2 2 0 012-2h2a2 2 0 012 2v10m-6 0a2 2 0 002 2h2a2 2 0 002-2m0 0V5a2 2 0 012-2h2a2 2 0 012 2v14a2 2 0 01-2 2h-2a2 2 0 01-2-2z"
+                />
+              </svg>
+            </MslTotalStatsItem>
+            <MslTotalStatsItem
+              title={`Total Mos`}
+              value={allSeasonStats.mos}
+            >
+              <svg
+                className="flex-shrink-0 h-6 w-6 text-indigo-600 dark:text-indigo-400"
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke="currentColor"
+                aria-hidden="true"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth={2}
+                  d="M9 19v-6a2 2 0 00-2-2H5a2 2 0 00-2 2v6a2 2 0 002 2h2a2 2 0 002-2zm0 0V9a2 2 0 012-2h2a2 2 0 012 2v10m-6 0a2 2 0 002 2h2a2 2 0 002-2m0 0V5a2 2 0 012-2h2a2 2 0 012 2v14a2 2 0 01-2 2h-2a2 2 0 01-2-2z"
+                />
+              </svg>
+            </MslTotalStatsItem>
+            <MslTotalStatsItem
+              title={`Total Yellow Cards`}
+              value={allSeasonStats.yellow_cards}
+            >
+              <svg
+                className="flex-shrink-0 h-6 w-6 text-indigo-600 dark:text-indigo-400"
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke="currentColor"
+                aria-hidden="true"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth={2}
+                  d="M9 19v-6a2 2 0 00-2-2H5a2 2 0 00-2 2v6a2 2 0 002 2h2a2 2 0 002-2zm0 0V9a2 2 0 012-2h2a2 2 0 012 2v10m-6 0a2 2 0 002 2h2a2 2 0 002-2m0 0V5a2 2 0 012-2h2a2 2 0 012 2v14a2 2 0 01-2 2h-2a2 2 0 01-2-2z"
+                />
+              </svg>
+            </MslTotalStatsItem>
+            <MslTotalStatsItem
+              title={`Total Red Cards`}
+              value={allSeasonStats.red_cards}
+            >
+              <svg
+                className="flex-shrink-0 h-6 w-6 text-indigo-600 dark:text-indigo-400"
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke="currentColor"
+                aria-hidden="true"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth={2}
+                  d="M9 19v-6a2 2 0 00-2-2H5a2 2 0 00-2 2v6a2 2 0 002 2h2a2 2 0 002-2zm0 0V9a2 2 0 012-2h2a2 2 0 012 2v10m-6 0a2 2 0 002 2h2a2 2 0 002-2m0 0V5a2 2 0 012-2h2a2 2 0 012 2v14a2 2 0 01-2 2h-2a2 2 0 01-2-2z"
+                />
+              </svg>
+            </MslTotalStatsItem>
+            <MslTotalStatsItem
+              title={`Total Mom`}
+              value={allSeasonStats.mom}
+            >
+              <svg
+                className="flex-shrink-0 h-6 w-6 text-indigo-600 dark:text-indigo-400"
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke="currentColor"
+                aria-hidden="true"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth={2}
+                  d="M9 19v-6a2 2 0 00-2-2H5a2 2 0 00-2 2v6a2 2 0 002 2h2a2 2 0 002-2zm0 0V9a2 2 0 012-2h2a2 2 0 012 2v10m-6 0a2 2 0 002 2h2a2 2 0 002-2m0 0V5a2 2 0 012-2h2a2 2 0 012 2v14a2 2 0 01-2 2h-2a2 2 0 01-2-2z"
+                />
+              </svg>
+            </MslTotalStatsItem>
+            <MslTotalStatsItem
+              title={`Total Players`}
+              value={allSeasonStats.players}
+            >
+              <svg
+                className="flex-shrink-0 h-6 w-6 text-indigo-600 dark:text-indigo-400"
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke="currentColor"
+                aria-hidden="true"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth={2}
+                  d="M9 19v-6a2 2 0 00-2-2H5a2 2 0 00-2 2v6a2 2 0 002 2h2a2 2 0 002-2zm0 0V9a2 2 0 012-2h2a2 2 0 012 2v10m-6 0a2 2 0 002 2h2a2 2 0 002-2m0 0V5a2 2 0 012-2h2a2 2 0 012 2v14a2 2 0 01-2 2h-2a2 2 0 01-2-2z"
+                />
+              </svg>
+            </MslTotalStatsItem>
+          </div>
+        </div>
       </div>
       <div>
         <h3>Seasons Played</h3>
