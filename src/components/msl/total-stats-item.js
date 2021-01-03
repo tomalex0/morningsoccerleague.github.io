5d3b3d0b3ec80a5266a72e2002199ce988e5d374
tomalex0 <tomalex0@gmail.com>
2021-01-03 09:57:33 -0800

    update teams page

--- src/components/msl/total-stats-item.js
+++ src/components/msl/total-stats-item.js
@@ -6,8 +6,10 @@ const MslTotalStatsItem = ({ title, value, children }) => {
     <div className="mt-2 flex">
       <div>{children}</div>
       <div className="ml-4">
-        <p className="text-base font-medium text-gray-900">{title}</p>
-        <p className="mt-1 text-sm text-gray-500">{value}</p>
+        <p className="text-base font-medium text-gray-900 dark:text-gray-300">
+          {title}
+        </p>
+        <p className="mt-1 text-sm text-gray-500 dark:text-gray-400">{value}</p>
       </div>
     </div>
   )
