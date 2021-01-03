212d3e7cc8cd542a4786474bb16f197806212821
tomalex0 <tomalex0@gmail.com>
2021-01-03 07:02:28 -0800

    implement season page

--- src/components/msl/season-stats-item.js
+++ src/components/msl/season-stats-item.js
@@ -3,7 +3,7 @@ import React from "react"
 
 const MslSeasonStatsItem = ({ season }) => {
   return (
-    <div className="lg:flex lg:items-center lg:justify-between">
+    <div className="lg:flex lg:items-center lg:justify-between px-10">
       <div className="flex-1 min-w-0">
         <h1 className="text-2xl font-bold leading-7 text-gray-600 dark:text-gray-300  sm:text-3xl sm:truncate">
           <Link to={season.seasonPath}>
@@ -13,184 +13,245 @@ const MslSeasonStatsItem = ({ season }) => {
         <div className="mt-1 flex flex-col sm:flex-row sm:flex-wrap sm:mt-0 sm:space-x-6">
           <div className="mt-2 flex items-center text-sm text-gray-500">
             <svg
-              className="flex-shrink-0 mr-1.5 h-8 w-8 text-gray-400"
+              className="flex-shrink-0 h-6 w-6 text-indigo-600"
               xmlns="http://www.w3.org/2000/svg"
-              viewBox="0 0 20 20"
-              fill="currentColor"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke="currentColor"
               aria-hidden="true"
             >
               <path
-                fillRule="evenodd"
-                d="M6 6V5a3 3 0 013-3h2a3 3 0 013 3v1h2a2 2 0 012 2v3.57A22.952 22.952 0 0110 13a22.95 22.95 0 01-8-1.43V8a2 2 0 012-2h2zm2-1a1 1 0 011-1h2a1 1 0 011 1v1H8V5zm1 5a1 1 0 011-1h.01a1 1 0 110 2H10a1 1 0 01-1-1z"
-                clipRule="evenodd"
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth={2}
+                d="M9 19v-6a2 2 0 00-2-2H5a2 2 0 00-2 2v6a2 2 0 002 2h2a2 2 0 002-2zm0 0V9a2 2 0 012-2h2a2 2 0 012 2v10m-6 0a2 2 0 002 2h2a2 2 0 002-2m0 0V5a2 2 0 012-2h2a2 2 0 012 2v14a2 2 0 01-2 2h-2a2 2 0 01-2-2z"
               />
-              <path d="M2 13.692V16a2 2 0 002 2h12a2 2 0 002-2v-2.308A24.974 24.974 0 0110 15c-2.796 0-5.487-.46-8-1.308z" />
             </svg>
-            {season.seasonStats.games}
+            <div className="ml-4">
+              <p className="text-base font-medium text-gray-900">Total Games</p>
+              <p className="mt-1 text-sm text-gray-500">
+                {season.seasonStats.games}
+              </p>
+            </div>
           </div>
           <div className="mt-2 flex items-center text-sm text-gray-500">
             <svg
-              className="flex-shrink-0 mr-1.5 h-8 w-8 text-gray-400"
+              className="flex-shrink-0 h-6 w-6 text-indigo-600"
               xmlns="http://www.w3.org/2000/svg"
-              viewBox="0 0 20 20"
-              fill="currentColor"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke="currentColor"
               aria-hidden="true"
             >
               <path
-                fillRule="evenodd"
-                d="M5.05 4.05a7 7 0 119.9 9.9L10 18.9l-4.95-4.95a7 7 0 010-9.9zM10 11a2 2 0 100-4 2 2 0 000 4z"
-                clipRule="evenodd"
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth={2}
+                d="M9 19v-6a2 2 0 00-2-2H5a2 2 0 00-2 2v6a2 2 0 002 2h2a2 2 0 002-2zm0 0V9a2 2 0 012-2h2a2 2 0 012 2v10m-6 0a2 2 0 002 2h2a2 2 0 002-2m0 0V5a2 2 0 012-2h2a2 2 0 012 2v14a2 2 0 01-2 2h-2a2 2 0 01-2-2z"
               />
             </svg>
-            {season.seasonStats.teams}
+            <div className="ml-4">
+              <p className="text-base font-medium text-gray-900">Total Teams</p>
+              <p className="mt-1 text-sm text-gray-500">
+                {season.seasonStats.teams}
+              </p>
+            </div>
           </div>
           <div className="mt-2 flex items-center text-sm text-gray-500">
             <svg
-              className="flex-shrink-0 mr-1.5 h-8 w-8 text-gray-400"
+              className="flex-shrink-0 h-6 w-6 text-indigo-600"
               xmlns="http://www.w3.org/2000/svg"
-              viewBox="0 0 20 20"
-              fill="currentColor"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke="currentColor"
               aria-hidden="true"
             >
-              <path d="M8.433 7.418c.155-.103.346-.196.567-.267v1.698a2.305 2.305 0 01-.567-.267C8.07 8.34 8 8.114 8 8c0-.114.07-.34.433-.582zM11 12.849v-1.698c.22.071.412.164.567.267.364.243.433.468.433.582 0 .114-.07.34-.433.582a2.305 2.305 0 01-.567.267z" />
               <path
-                fillRule="evenodd"
-                d="M10 18a8 8 0 100-16 8 8 0 000 16zm1-13a1 1 0 10-2 0v.092a4.535 4.535 0 00-1.676.662C6.602 6.234 6 7.009 6 8c0 .99.602 1.765 1.324 2.246.48.32 1.054.545 1.676.662v1.941c-.391-.127-.68-.317-.843-.504a1 1 0 10-1.51 1.31c.562.649 1.413 1.076 2.353 1.253V15a1 1 0 102 0v-.092a4.535 4.535 0 001.676-.662C13.398 13.766 14 12.991 14 12c0-.99-.602-1.765-1.324-2.246A4.535 4.535 0 0011 9.092V7.151c.391.127.68.317.843.504a1 1 0 101.511-1.31c-.563-.649-1.413-1.076-2.354-1.253V5z"
-                clipRule="evenodd"
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth={2}
+                d="M9 19v-6a2 2 0 00-2-2H5a2 2 0 00-2 2v6a2 2 0 002 2h2a2 2 0 002-2zm0 0V9a2 2 0 012-2h2a2 2 0 012 2v10m-6 0a2 2 0 002 2h2a2 2 0 002-2m0 0V5a2 2 0 012-2h2a2 2 0 012 2v14a2 2 0 01-2 2h-2a2 2 0 01-2-2z"
               />
             </svg>
-            {season.seasonStats.players}
+            <div className="ml-4">
+              <p className="text-base font-medium text-gray-900">
+                Total Players
+              </p>
+              <p className="mt-1 text-sm text-gray-500">
+                {season.seasonStats.players}
+              </p>
+            </div>
           </div>
           <div className="mt-2 flex items-center text-sm text-gray-500">
             <svg
-              className="flex-shrink-0 mr-1.5 h-8 w-8 text-gray-400"
+              className="flex-shrink-0 h-6 w-6 text-indigo-600"
               xmlns="http://www.w3.org/2000/svg"
-              viewBox="0 0 20 20"
-              fill="currentColor"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke="currentColor"
               aria-hidden="true"
             >
               <path
-                fillRule="evenodd"
-                d="M6 2a1 1 0 00-1 1v1H4a2 2 0 00-2 2v10a2 2 0 002 2h12a2 2 0 002-2V6a2 2 0 00-2-2h-1V3a1 1 0 10-2 0v1H7V3a1 1 0 00-1-1zm0 5a1 1 0 000 2h8a1 1 0 100-2H6z"
-                clipRule="evenodd"
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth={2}
+                d="M9 19v-6a2 2 0 00-2-2H5a2 2 0 00-2 2v6a2 2 0 002 2h2a2 2 0 002-2zm0 0V9a2 2 0 012-2h2a2 2 0 012 2v10m-6 0a2 2 0 002 2h2a2 2 0 002-2m0 0V5a2 2 0 012-2h2a2 2 0 012 2v14a2 2 0 01-2 2h-2a2 2 0 01-2-2z"
               />
             </svg>
-            {season.seasonStats.goals}
+            <div className="ml-4">
+              <p className="text-base font-medium text-gray-900">Total Goals</p>
+              <p className="mt-1 text-sm text-gray-500">
+                {season.seasonStats.goals}
+              </p>
+            </div>
           </div>
           <div className="mt-2 flex items-center text-sm text-gray-500">
             <svg
-              className="flex-shrink-0 mr-1.5 h-8 w-8 text-gray-400"
+              className="flex-shrink-0 h-6 w-6 text-indigo-600"
               xmlns="http://www.w3.org/2000/svg"
-              viewBox="0 0 20 20"
-              fill="currentColor"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke="currentColor"
               aria-hidden="true"
             >
               <path
-                fillRule="evenodd"
-                d="M6 2a1 1 0 00-1 1v1H4a2 2 0 00-2 2v10a2 2 0 002 2h12a2 2 0 002-2V6a2 2 0 00-2-2h-1V3a1 1 0 10-2 0v1H7V3a1 1 0 00-1-1zm0 5a1 1 0 000 2h8a1 1 0 100-2H6z"
-                clipRule="evenodd"
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth={2}
+                d="M9 19v-6a2 2 0 00-2-2H5a2 2 0 00-2 2v6a2 2 0 002 2h2a2 2 0 002-2zm0 0V9a2 2 0 012-2h2a2 2 0 012 2v10m-6 0a2 2 0 002 2h2a2 2 0 002-2m0 0V5a2 2 0 012-2h2a2 2 0 012 2v14a2 2 0 01-2 2h-2a2 2 0 01-2-2z"
               />
             </svg>
-            {season.seasonStats.assists}
+            <div className="ml-4">
+              <p className="text-base font-medium text-gray-900">Total Fouls</p>
+              <p className="mt-1 text-sm text-gray-500">
+                {season.seasonStats.fouls}
+              </p>
+            </div>
           </div>
           <div className="mt-2 flex items-center text-sm text-gray-500">
             <svg
-              className="flex-shrink-0 mr-1.5 h-8 w-8 text-gray-400"
+              className="flex-shrink-0 h-6 w-6 text-indigo-600"
               xmlns="http://www.w3.org/2000/svg"
-              viewBox="0 0 20 20"
-              fill="currentColor"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke="currentColor"
               aria-hidden="true"
             >
               <path
-                fillRule="evenodd"
-                d="M6 2a1 1 0 00-1 1v1H4a2 2 0 00-2 2v10a2 2 0 002 2h12a2 2 0 002-2V6a2 2 0 00-2-2h-1V3a1 1 0 10-2 0v1H7V3a1 1 0 00-1-1zm0 5a1 1 0 000 2h8a1 1 0 100-2H6z"
-                clipRule="evenodd"
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth={2}
+                d="M9 19v-6a2 2 0 00-2-2H5a2 2 0 00-2 2v6a2 2 0 002 2h2a2 2 0 002-2zm0 0V9a2 2 0 012-2h2a2 2 0 012 2v10m-6 0a2 2 0 002 2h2a2 2 0 002-2m0 0V5a2 2 0 012-2h2a2 2 0 012 2v14a2 2 0 01-2 2h-2a2 2 0 01-2-2z"
               />
             </svg>
-            {season.seasonStats.owngoals}
+            <div className="ml-4">
+              <p className="text-base font-medium text-gray-900">Own Goals</p>
+              <p className="mt-1 text-sm text-gray-500">
+                {season.seasonStats.owngoals}
+              </p>
+            </div>
           </div>
           <div className="mt-2 flex items-center text-sm text-gray-500">
             <svg
-              className="flex-shrink-0 mr-1.5 h-8 w-8 text-gray-400"
+              className="flex-shrink-0 h-6 w-6 text-indigo-600"
               xmlns="http://www.w3.org/2000/svg"
-              viewBox="0 0 20 20"
-              fill="currentColor"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke="currentColor"
               aria-hidden="true"
             >
               <path
-                fillRule="evenodd"
-                d="M6 2a1 1 0 00-1 1v1H4a2 2 0 00-2 2v10a2 2 0 002 2h12a2 2 0 002-2V6a2 2 0 00-2-2h-1V3a1 1 0 10-2 0v1H7V3a1 1 0 00-1-1zm0 5a1 1 0 000 2h8a1 1 0 100-2H6z"
-                clipRule="evenodd"
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth={2}
+                d="M9 19v-6a2 2 0 00-2-2H5a2 2 0 00-2 2v6a2 2 0 002 2h2a2 2 0 002-2zm0 0V9a2 2 0 012-2h2a2 2 0 012 2v10m-6 0a2 2 0 002 2h2a2 2 0 002-2m0 0V5a2 2 0 012-2h2a2 2 0 012 2v14a2 2 0 01-2 2h-2a2 2 0 01-2-2z"
               />
             </svg>
-            {season.seasonStats.assists}
+            <div className="ml-4">
+              <p className="text-base font-medium text-gray-900">
+                Total Assists
+              </p>
+              <p className="mt-1 text-sm text-gray-500">
+                {season.seasonStats.assists}
+              </p>
+            </div>
           </div>
           <div className="mt-2 flex items-center text-sm text-gray-500">
             <svg
-              className="flex-shrink-0 mr-1.5 h-8 w-8 text-gray-400"
+              className="flex-shrink-0 h-6 w-6 text-indigo-600"
               xmlns="http://www.w3.org/2000/svg"
-              viewBox="0 0 20 20"
-              fill="currentColor"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke="currentColor"
               aria-hidden="true"
             >
               <path
-                fillRule="evenodd"
-                d="M6 2a1 1 0 00-1 1v1H4a2 2 0 00-2 2v10a2 2 0 002 2h12a2 2 0 002-2V6a2 2 0 00-2-2h-1V3a1 1 0 10-2 0v1H7V3a1 1 0 00-1-1zm0 5a1 1 0 000 2h8a1 1 0 100-2H6z"
-                clipRule="evenodd"
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth={2}
+                d="M9 19v-6a2 2 0 00-2-2H5a2 2 0 00-2 2v6a2 2 0 002 2h2a2 2 0 002-2zm0 0V9a2 2 0 012-2h2a2 2 0 012 2v10m-6 0a2 2 0 002 2h2a2 2 0 002-2m0 0V5a2 2 0 012-2h2a2 2 0 012 2v14a2 2 0 01-2 2h-2a2 2 0 01-2-2z"
               />
             </svg>
-            {season.seasonStats.unique_players_goals}
+            <div className="ml-4">
+              <p className="text-base font-medium text-gray-900">Total Goals</p>
+              <p className="mt-1 text-sm text-gray-500">
+                {season.seasonStats.unique_players_goals}
+              </p>
+            </div>
           </div>
           <div className="mt-2 flex items-center text-sm text-gray-500">
             <svg
-              className="flex-shrink-0 mr-1.5 h-8 w-8 text-gray-400"
+              className="flex-shrink-0 h-6 w-6 text-indigo-600"
               xmlns="http://www.w3.org/2000/svg"
-              viewBox="0 0 20 20"
-              fill="currentColor"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke="currentColor"
               aria-hidden="true"
             >
               <path
-                fillRule="evenodd"
-                d="M6 2a1 1 0 00-1 1v1H4a2 2 0 00-2 2v10a2 2 0 002 2h12a2 2 0 002-2V6a2 2 0 00-2-2h-1V3a1 1 0 10-2 0v1H7V3a1 1 0 00-1-1zm0 5a1 1 0 000 2h8a1 1 0 100-2H6z"
-                clipRule="evenodd"
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth={2}
+                d="M9 19v-6a2 2 0 00-2-2H5a2 2 0 00-2 2v6a2 2 0 002 2h2a2 2 0 002-2zm0 0V9a2 2 0 012-2h2a2 2 0 012 2v10m-6 0a2 2 0 002 2h2a2 2 0 002-2m0 0V5a2 2 0 012-2h2a2 2 0 012 2v14a2 2 0 01-2 2h-2a2 2 0 01-2-2z"
               />
             </svg>
-            {season.seasonStats.yellow_cards}
+            <div className="ml-4">
+              <p className="text-base font-medium text-gray-900">
+                Total Yellow Cards
+              </p>
+              <p className="mt-1 text-sm text-gray-500">
+                {season.seasonStats.yellow_cards}
+              </p>
+            </div>
           </div>
           <div className="mt-2 flex items-center text-sm text-gray-500">
             <svg
-              className="flex-shrink-0 mr-1.5 h-8 w-8 text-gray-400"
+              className="flex-shrink-0 h-6 w-6 text-indigo-600"
               xmlns="http://www.w3.org/2000/svg"
-              viewBox="0 0 20 20"
-              fill="currentColor"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke="currentColor"
               aria-hidden="true"
             >
               <path
-                fillRule="evenodd"
-                d="M6 2a1 1 0 00-1 1v1H4a2 2 0 00-2 2v10a2 2 0 002 2h12a2 2 0 002-2V6a2 2 0 00-2-2h-1V3a1 1 0 10-2 0v1H7V3a1 1 0 00-1-1zm0 5a1 1 0 000 2h8a1 1 0 100-2H6z"
-                clipRule="evenodd"
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth={2}
+                d="M9 19v-6a2 2 0 00-2-2H5a2 2 0 00-2 2v6a2 2 0 002 2h2a2 2 0 002-2zm0 0V9a2 2 0 012-2h2a2 2 0 012 2v10m-6 0a2 2 0 002 2h2a2 2 0 002-2m0 0V5a2 2 0 012-2h2a2 2 0 012 2v14a2 2 0 01-2 2h-2a2 2 0 01-2-2z"
               />
             </svg>
-            {season.seasonStats.red_cards}
+            <div className="ml-4">
+              <p className="text-base font-medium text-gray-900">
+                Total Red Cards
+              </p>
+              <p className="mt-1 text-sm text-gray-500">
+                {season.seasonStats.red_cards}
+              </p>
+            </div>
           </div>
         </div>
       </div>
     </div>
-
-    // <li>
-    //   <Link to={season.seasonPath}>
-    //     {season.season_id}-{season.season_year}-{season.seasonStats.games}{" "}
-    //     Game--{season.seasonStats.teams} Teams -- {season.seasonStats.players}{" "}
-    //     Players -- {season.seasonStats.goals}Goals --{" "}
-    //     {season.seasonStats.assists}
-    //     Assists -- {season.seasonStats.owngoals}
-    //     Owngoals -- {season.seasonStats.fouls}
-    //     Total Fouls -- {season.seasonStats.unique_players_goals}
-    //     Unique Players Scored -- {season.seasonStats.yellow_cards}
-    //     Yellow Card -- {season.seasonStats.red_cards} Red Card --
-    //     {season.mos.map(item => item.name).join(",")} Mos
-    //   </Link>
-    // </li>
   )
 }
 
