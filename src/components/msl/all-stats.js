3c9b1eb6ac29ae3e49f9aa9eca5328461b85dfd9
tomalex0 <tomalex0@gmail.com>
2021-01-04 10:24:15 -0800

    update style

--- src/components/msl/all-stats.js
+++ src/components/msl/all-stats.js
@@ -13,6 +13,23 @@ const MslAllStats = ({ player }) => {
         </div>
         <div className="mt-10">
           <dl className="space-y-10 md:space-y-0 md:grid md:grid-cols-4 md:gap-x-8 md:gap-y-10">
+            <MslAllStatsItem title="Seasons" value="3">
+              <svg
+                className="h-6 w-6"
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke="currentColor"
+                aria-hidden="true"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth={2}
+                  d="M9 19v-6a2 2 0 00-2-2H5a2 2 0 00-2 2v6a2 2 0 002 2h2a2 2 0 002-2zm0 0V9a2 2 0 012-2h2a2 2 0 012 2v10m-6 0a2 2 0 002 2h2a2 2 0 002-2m0 0V5a2 2 0 012-2h2a2 2 0 012 2v14a2 2 0 01-2 2h-2a2 2 0 01-2-2z"
+                />
+              </svg>
+            </MslAllStatsItem>
             <MslAllStatsItem title="Games" value="43">
               <svg
                 className="h-6 w-6"
--- src/components/msl/team-members.js
+++ src/components/msl/team-members.js
@@ -6,7 +6,7 @@ const MslTeamMembers = response => {
   const { team, players } = response.data
 
   return (
-    <div className="bg-white shadow overflow-hidden sm:rounded-lg">
+    <div className="bg-white shadow overflow-hidden rounded-lg">
       <div className="px-4 py-5 sm:px-6">
         <h3 className="text-lg leading-6 font-medium text-gray-900 flex items-center">
           <MslTeamItem team={team} />{" "}
