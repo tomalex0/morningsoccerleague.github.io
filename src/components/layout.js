c9b2c1de9b1dc60ca6cb995411894d1b17b2e851
tomalex0 <tomalex0@gmail.com>
2021-01-05 02:32:51 -0800

    update footer

--- src/components/layout.js
+++ src/components/layout.js
@@ -12,6 +12,8 @@ import { useStaticQuery, graphql } from "gatsby"
 import Header from "./header"
 import MslContact from "components/msl/contact"
 
+import FbLogo from "images/social/facebook.svg"
+
 const Layout = ({ children }) => {
   const data = useStaticQuery(graphql`
     query SiteTitleQuery {
@@ -30,8 +32,29 @@ const Layout = ({ children }) => {
         <main className="flex-grow mt-16 bg-gray-50 dark:bg-gray-700 pb-10">
           {children}
         </main>
-        <footer>
+        <footer className="bg-gray-100 dark:bg-gray-800">
           <MslContact />
+          <div className="pt-2">
+            <div
+              className="flex pb-5 px-3 m-auto pt-5
+      border-t border-gray-500 text-gray-400 text-sm
+      flex-col md:flex-row max-w-6xl"
+            >
+              <div className="mt-2">
+                Copyright: © {new Date().getFullYear()} Morning Soccer League.
+                All Rights Reserved.
+              </div>
+              <div className="md:flex-auto md:flex-row-reverse mt-2 flex-row flex">
+                <a
+                  href="https://www.facebook.com/morningsoccerleague/"
+                  target="_blank"
+                  className="w-8 mx-1"
+                >
+                  <img alt="Facebook" src={FbLogo} />
+                </a>
+              </div>
+            </div>
+          </div>
         </footer>
       </div>
     </>
--- src/components/msl/contact.js
+++ src/components/msl/contact.js
@@ -3,7 +3,7 @@ import React from "react"
 
 const MslContact = () => {
   return (
-    <div className="bg-gray-100 dark:bg-gray-800">
+    <div>
       <div className="max-w-7xl mx-auto py-12 px-4 sm:px-6 lg:py-16 lg:px-8 lg:flex lg:items-center lg:justify-between">
         <h2 className="text-3xl font-extrabold tracking-tight text-gray-900 sm:text-4xl">
           <span className="block text-indigo-600 dark:text-indigo-400">
