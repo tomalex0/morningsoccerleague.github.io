a5bcc0bf422f15a5116c16499b785e9ac04f5b8e
tomalex0 <tomalex0@gmail.com>
2021-01-08 02:15:33 -0800

    change style switcher

--- src/components/msl/theme-switch.js
+++ src/components/msl/theme-switch.js
@@ -8,10 +8,10 @@ const MslThemeSwitch = props => {
   const toggleState = () => setToggleActive(value => !value)
 
   const handleThemeToggle = () => {
-    if (theme === "light") {
-      setTheme("dark")
-    } else {
+    if (theme === "dark") {
       setTheme("light")
+    } else {
+      setTheme("dark")
     }
   }
 
@@ -34,7 +34,7 @@ const MslThemeSwitch = props => {
             stroke="currentColor"
             className="w-6"
           >
-            <g className="opacity-0 dark:opacity-100">
+            <g className="transition delay-200 duration-200 ease-in-out opacity-0 dark:opacity-100">
               <path
                 strokeLinecap="round"
                 strokeLinejoin="round"
@@ -42,7 +42,7 @@ const MslThemeSwitch = props => {
                 d="M12 3v1m0 16v1m9-9h-1M4 12H3m15.364 6.364l-.707-.707M6.343 6.343l-.707-.707m12.728 0l-.707.707M6.343 17.657l-.707.707M16 12a4 4 0 11-8 0 4 4 0 018 0z"
               />
             </g>
-            <g className="dark:opacity-0">
+            <g className="transition delay-200 duration-200 ease-in-out dark:opacity-0">
               <path
                 strokeLinecap="round"
                 strokeLinejoin="round"
