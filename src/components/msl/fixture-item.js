88b051be4dc92edb20d6b1bf78e2bf7c1bb82f52
tomalex0 <tomalex0@gmail.com>
2021-01-09 03:12:34 -0800

    update fixtures page

--- src/components/msl/fixture-item.js
+++ src/components/msl/fixture-item.js
@@ -2,11 +2,14 @@ import { Link } from "gatsby"
 import React from "react"
 
 const MslFixtureItem = ({ fixture }) => {
+  console.log(fixture)
   return (
     <div className="flex space-x-0 text-gray-900 bg-gray-50 dark:bg-gray-300 rounded-sm shadow-2xl shadow-inner mb-4 p-2">
       <div className="rounded-l-md flex-grow space-x-1  flex-shrink-0 w-16 text-sm md:text-base flex items-center justify-center">
         <div className="flex-grow h-16 rounded-md font-bold flex items-center justify-end pl-2">
-          {fixture?.home?.team?.teamName}
+          <Link to={fixture?.home?.team?.teamPath}>
+            {fixture?.home?.team?.teamName}
+          </Link>
         </div>
         <div className="flex-none w-10 md:w-16 text-2xl font-extrabold flex items-center justify-center">
           <img
@@ -32,7 +35,9 @@ const MslFixtureItem = ({ fixture }) => {
           />
         </div>
         <div className="flex-grow h-16 rounded-md font-bold flex items-center justify-start pr-2">
-          {fixture?.away?.team?.teamName}
+          <Link to={fixture?.away?.team?.teamPath}>
+            {fixture?.away?.team?.teamName}
+          </Link>
         </div>
       </div>
     </div>
