959f308ecafd23432c325ab6a4a5e3fe60658f89
tomalex0 <tomalex0@gmail.com>
2021-01-08 21:09:11 -0800

    fix styles

--- src/pages/players/index.js
+++ src/pages/players/index.js
@@ -139,32 +139,8 @@ const PlayersIndex = ({ data, path }) => {
               <option value="1">DESC</option>
             </select>
           </form>
-
-          {/*<div className="relative w-full md:w-3/6 mx-auto">*/}
-          {/*  <svg*/}
-          {/*    width="20"*/}
-          {/*    height="20"*/}
-          {/*    fill="currentColor"*/}
-          {/*    className="absolute left-3 top-1/2 transform -translate-y-1/2 text-gray-400"*/}
-          {/*  >*/}
-          {/*    <path*/}
-          {/*      fill-rule="evenodd"*/}
-          {/*      clip-rule="evenodd"*/}
-          {/*      d="M8 4a4 4 0 100 8 4 4 0 000-8zM2 8a6 6 0 1110.89 3.476l4.817 4.817a1 1 0 01-1.414 1.414l-4.816-4.816A6 6 0 012 8z"*/}
-          {/*    />*/}
-          {/*  </svg>*/}
-
-          {/*  <input*/}
-          {/*    type="text"*/}
-          {/*    className="focus:border-light-indigo-500 focus:ring-1 focus:ring-light-blue-500 focus:outline-none w-full text-sm text-black placeholder-gray-500 border border-gray-200 rounded-md py-2 pl-10  shadow-md"*/}
-          {/*    aria-label="Search"*/}
-          {/*    placeholder="Type to filter players..."*/}
-          {/*    onChange={handleInputChange}*/}
-          {/*  />*/}
-          {/*</div>*/}
           <div className="mt-2 text-gray-600 dark:text-gray-300 text-left">
-            {playerList.length} Results {searchQuery} {sortState.toString()}{" "}
-            {orderState.toString()}
+            {playerList.length} Results
           </div>
         </div>
         <div>
