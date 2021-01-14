b0d31d2c31c153e895ae4d28a037873346e554ce
tomalex0 <tomalex0@gmail.com>
2021-01-13 22:55:12 -0800

    season section updated

--- src/components/msl/table-header-cell.js
+++ src/components/msl/table-header-cell.js
@@ -5,7 +5,7 @@ const MslTableHeaderCell = ({ children, cssName }) => {
   return (
     <th
       scope="col"
-      className={`px-4 py-3 text-left text-xs font-medium text-gray-500 dark:text-gray-200 uppercase tracking-wider ${cssName}`}
+      className={`px-2 py-2 text-left text-xs font-medium text-gray-500 dark:text-gray-200 uppercase tracking-wider ${cssName}`}
     >
       {children}
     </th>
