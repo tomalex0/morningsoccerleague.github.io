c61c06f08177d14fb7bf0934acef79178271ef96
tomalex0 <tomalex0@gmail.com>
2021-01-16 00:04:56 -0800

    update title

--- src/pages/gmm.js
+++ src/pages/gmm.js
@@ -11,7 +11,7 @@ const IndexPage = ({ path }) => {
   const isSSR = typeof window === "undefined"
   return (
     <Layout>
-      <SEO title="Register" path={path} />
+      <SEO title="GMM" path={path} />
       <div className=" mx-auto px-5 mt-10">
         <h1 className="text-2xl font-bold leading-7 text-dark-600 dark:text-dark-300  sm:text-3xl sm:truncate mb-4">
           GMM
