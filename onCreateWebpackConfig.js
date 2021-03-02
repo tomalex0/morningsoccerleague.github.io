441e3c243037c815ce082f548d504d1241605e21
tomalex0 <tomalex0@gmail.com>
2021-03-02 12:55:48 -0800

    update  node module

--- onCreateWebpackConfig.js
+++ onCreateWebpackConfig.js
@@ -2,6 +2,12 @@ const { resolve } = require("path-browserify")
 module.exports = ({ actions }) => {
   actions.setWebpackConfig({
     resolve: {
+      fallback: {
+        fs: false
+      },
+      alias: {
+        path: require.resolve("path-browserify")
+      },
       modules: [resolve(__dirname, "src"), "node_modules"],
     },
   })
