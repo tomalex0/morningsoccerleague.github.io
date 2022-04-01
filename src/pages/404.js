61f6dad37f621619fa628411282f50b8f2c2018c
tomalex0 <tomalex0@gmail.com>
2022-03-31 17:37:46 -0700

    remove gatsby tina cms

--- src/pages/404.js
+++ src/pages/404.js
@@ -7,16 +7,14 @@ import { graphql } from "gatsby"
 import Layout from "components/layout"
 import SEO from "components/seo"
 
-const NotFoundPage = ({ path, data }) => {
-  const { markdownRemark } = data
-  const { frontmatter, html } = markdownRemark
+const NotFoundPage = ({ path }) => {
   return (
     <Layout>
-      <SEO title={frontmatter.title} path={path} />
+      <SEO title="404" path={path} />
       <div className="flex h-screen justify-center items-center ">
         <div className="text-center">
           <p className="text-9xl text-dark-700 dark:text-dark-200 ">
-            {frontmatter.description}
+            404
           </p>
         </div>
       </div>
@@ -24,34 +22,5 @@ const NotFoundPage = ({ path, data }) => {
   )
 }
 
-export const pageQuery = graphql`
-  query {
-    markdownRemark(frontmatter: { slug: { eq: "404" } }) {
-      html
-      ...TinaRemark
-      frontmatter {
-        description
-        slug
-        title
-      }
-    }
-  }
-`
-const PageForm = {
-  fields: [
-    {
-      label: "Title",
-      name: "frontmatter.title",
-      description: "Enter the title of the page here",
-      component: "text",
-    },
-    {
-      label: "Description",
-      name: "frontmatter.description",
-      description: "Enter the page description",
-      component: "textarea",
-    },
-  ],
-}
 
-export default remarkForm(NotFoundPage, PageForm)
+export default NotFoundPage
